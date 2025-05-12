308df80ed8c45b29c4bf121e0b3910184b59a1fd
Joyce431 <koetlenthabeleng431@gmail.com>
2025-05-12 16:06:12 +0200

    Add files via upload

---
+++ project6.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+# Replace these with your actual database credentials
+connection = psycopg2.connect(
+    host="localhost",
+    port="5432",
+    database="shop",
+    user="postgres",
+    password="moabi1608"
+)
+
+# 3. Select and display data from 'users' table
+cursor = connection.cursor()
+cursor.execute("SELECT * FROM saleitem;")
+dataset= cursor.fetchall()
+print("shop table data:")
+for data in dataset:
+    print(data)
+
+# Cleanup
+cursor.close()
+connection.close()
